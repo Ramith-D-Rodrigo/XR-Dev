b59d3554baf8f34d7335b3705cb86cc21b2da37f
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2024-08-01 18:53:07 +0530

    Add textures and manipulate lighting

--- Third App/index.js
+++ Third App/index.js
@@ -17,9 +17,17 @@ function main() {
 
     // create the scene
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color('white');
+
+    const fog = new THREE.Fog(0x262837, 1, 100);
+    scene.fog = fog;
 
     // GEOEMTRY
         // upright plane
+    const planeWidth = 256;
+    const planeHeight = 256;
+
+    const planeGeo = new THREE.PlaneGeometry(planeWidth, planeHeight);
         // cube
     const cubeSize = 4;
     const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
@@ -32,25 +40,94 @@ function main() {
     // MATERIAL
     const cubeMaterial = new THREE.MeshPhongMaterial({color: 'red'});
     const sphereMaterial = new THREE.MeshPhongMaterial({color: 'tan'});
+    
+    const textureLoader = new THREE.TextureLoader();
+
+    const planeTextureMap = textureLoader.load('textures/pebbles/Pebbles_024_BaseColor.jpg');
+    const planeNormalMap = textureLoader.load('textures/pebbles/Pebbles_024_Normal.jpg');
+    const planeRoughnessMap = textureLoader.load('textures/pebbles/Pebbles_024_Roughness.jpg');
+    const planeDisplacementMap = textureLoader.load('textures/pebbles/Pebbles_024_Height.png');
+    const ambientOcclusionMap = textureLoader.load('textures/pebbles/Pebbles_024_AmbientOcclusion.jpg');
+
+    //texture map wrapping
+    planeTextureMap.wrapS = THREE.RepeatWrapping;
+    planeTextureMap.wrapT = THREE.RepeatWrapping;
+    planeTextureMap.repeat.set(16, 16);
+
+    planeNormalMap.wrapS = THREE.RepeatWrapping;
+    planeNormalMap.wrapT = THREE.RepeatWrapping;
+    planeNormalMap.repeat.set(16, 16);
+
+    planeRoughnessMap.wrapS = THREE.RepeatWrapping;
+    planeRoughnessMap.wrapT = THREE.RepeatWrapping;
+    planeRoughnessMap.repeat.set(16, 16);
+
+    planeDisplacementMap.wrapS = THREE.RepeatWrappming;
+    planeDisplacementMap.wrapT = THREE.RepeatWrapping;
+    planeDisplacementMap.repeat.set(16, 16);
+
+    ambientOcclusionMap.wrapS = THREE.RepeatWrapping;
+    ambientOcclusionMap.wrapT = THREE.RepeatWrapping;
+    ambientOcclusionMap.repeat.set(16, 16);
+
+    //mipmapping
+    planeTextureMap.minFilter = THREE.NearestFilter;
+    planeNormalMap.minFilter = THREE.NearestFilter;
+    planeRoughnessMap.minFilter = THREE.NearestFilter;
+    planeDisplacementMap.minFilter = THREE.NearestFilter;
+    ambientOcclusionMap.minFilter = THREE.NearestFilter;
+
+    //set anisotropy
+    planeTextureMap.anisotropy = gl.getMaxAnisotropy();
+    planeNormalMap.anisotropy = gl.getMaxAnisotropy();
+    planeRoughnessMap.anisotropy = gl.getMaxAnisotropy();
+    planeDisplacementMap.anisotropy = gl.getMaxAnisotropy();
+    ambientOcclusionMap.anisotropy = gl.getMaxAnisotropy();
+
+
+    const planeMaterial = new THREE.MeshStandardMaterial({
+        map: planeTextureMap,
+        normalMap: planeNormalMap,
+        displacementMap: planeDisplacementMap,
+        aoMap: ambientOcclusionMap,
+        displacementScale: 0.5,
+        displacementBias: -0,
+        side: THREE.DoubleSide
+    });
+
+    
 
     // LIGHT
     const lightColor = 0xFFFFFF;
     const lightIntensity = 1;
     const light = new THREE.DirectionalLight(lightColor, lightIntensity);
+    light.position.set(0, 30, 30);
     scene.add(light);
+
+    const ambientColor = 0xFFFFFF;
+    const ambientIntensity = 0.3;
+    const ambientLight = new THREE.AmbientLight(ambientColor, ambientIntensity);
+    scene.add(ambientLight);
 
     // MESH
     const cube = new THREE.Mesh(cubeGeo, cubeMaterial);
-    cube.position.set(0, 0, 0);
+    cube.position.set(cubeSize, cubeSize, 0);
     scene.add(cube);
 
     const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
     sphere.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
     scene.add(sphere);
 
+    const plane = new THREE.Mesh(planeGeo, planeMaterial);
+    plane.rotation.x = Math.PI / 2;
+    scene.add(plane);
+
+    light.target = plane; //tilt the light towards the plane
+    scene.add(light.target);
 
     // RENDER/DRAW
-    function draw() {
+    function draw(time) {
+        time *= 0.001;//convert time to seconds
 
         if(resizeGLToDisplaySize(gl)) {
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -63,6 +140,9 @@ function main() {
         sphere.rotation.x += 0.01;
         sphere.rotation.y += 0.01;
         sphere.rotation.y += 0.01;
+
+        light.position.x = 20 * Math.cos(time);
+        light.position.y = 20 * Math.sin(time);
 
         gl.render(scene, camera);
         requestAnimationFrame(draw);
